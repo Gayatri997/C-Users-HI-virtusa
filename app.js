1dc0bf856e8b22f5a506cf5b300420ee5c0d9511
Gayatri997 <43266470+Gayatri997@users.noreply.github.com>
2018-09-19 14:48:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+
+function identifyById()
+{
+    document.getElementById("para1").innerHTML="identified";
+}
+function identifyByClass()
+{
+    document.getElementsByClassName("class1")[0].innerHTML="identified";
+}
+function byTag()
+{
+    document.getElementsByTagName("p")[2].innerHTML="identified";
+}
 function changeBackground()
 {
     document.getElementById("block1").style.background="blue";
@@ -6,17 +19,24 @@ function increaseFont()
 {
     document.getElementById("block2").style.fontSize = "xx-large";
 }
-setInterval(function()
-{
-        var x = document.getElementById("block3");
-        var y = document.getElementById("block3");
-    
+
         function changeFontColor() {
-            x.style.color = 'green';
-            y.style.color = 'black';
-        }
-    
-        function revertColor() {
-            x.style.color = 'black';
-        }
-    },250);
+    document.getElementById("block3").style.color = "green";
+}
+
+function revertColor() {
+    document.getElementById("block3").style.color = "black";
+}
+document.getElementById("block4").ondblclick=function() {hide()};
+    function hide() {
+        document.getElementById("block4").innerHTML =" ";
+    }
+function myFunction()
+   {
+       var a=document.getElementById("fname");
+       a.value=a.value.toUpperCase();
+   }
+function color(x)
+    {
+  x.style.background="green";
+    }
